fb79e109b6afe2488dfce449e7fe1942129c9d01
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-06-15 13:39:45 -0300

    chore: update dockerfile and compose file

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
 FROM golang
 
+WORKDIR /go/src/helpy
+
 COPY . /go/src/helpy
 
-RUN go mod download
-RUN go install helpy
-
-ENTRYPOINT /go/bin/helpy
+RUN go mod download && go build -o ./bin ./cmd/api/main.go
 
 EXPOSE 5579
+
+ENTRYPOINT /go/src/helpy/bin/main
--- infra/validation/user.go
+++ infra/validation/user.go
@@ -7,21 +7,17 @@ import (
 	"github.com/asaskevich/govalidator"
 )
 
-func ErrInvalidField(field string) error {
-	return fmt.Errorf("invalid field %s", field)
-}
-
 type UserGoValidator struct{}
 
 func (v UserGoValidator) Validate(data *entities.User) error {
 	if !govalidator.IsUUIDv4(data.Id) {
-		return ErrInvalidField("id")
+		return fmt.Errorf("invalid field %s", "id")
 
 	} else if !govalidator.IsEmail(data.Email) {
-		return ErrInvalidField("email")
+		return fmt.Errorf("invalid field %s", "email")
 
 	} else if !govalidator.IsURL(data.AvatarUrl) {
-		return ErrInvalidField("avatar_url")
+		return fmt.Errorf("invalid field %s", "avatar_url")
 
 	}
 
