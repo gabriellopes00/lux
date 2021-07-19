b6cc915de0410e880bef7323a8960cf27d410a7e
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-07-19 19:12:35 -0300

    feat: create article model

---
+++ pkg/entities/article.go
@@ -0,0 +1,13 @@
+package entities
+
+import "time"
+
+type Article struct {
+	Id        string    `json:"id,omitempty" gorm:"primary_key"`
+	Title     string    `json:"title,omitempty" gorm:"not null; type:text"`
+	Content   string    `json:"content,omitempty" gorm:"not null; type:text"`
+	UserId    string    `json:"-" gorm:"not null; type:varchar"`
+	//Tags       []Tag    `json:"tags,omitempty" gorm:"type:text"`
+	CreatedAt time.Time `json:"created_at,omitempty" gorm:"not null"`
+	UpdatedAt time.Time `json:"updated_at,omitempty" gorm:"not null"`
+}
---
+++ pkg/entities/tag.go
@@ -0,0 +1,3 @@
+package entities
+
+type Tag string
